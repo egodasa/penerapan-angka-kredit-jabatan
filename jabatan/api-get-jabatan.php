f3f3d35f5571044de42515e54d986ee89e002cc2
Ego Dafma Dasa <egodasa@gmail.com>
2019-05-01 19:12:06 +0700

    Penambahan api untuk posisi dan jabatan

---
+++ jabatan/api-get-jabatan.php
@@ -0,0 +1,23 @@
+<?php
+  session_start();
+  header('Content-Type: application/json');
+  
+  require_once("../vendor/autoload.php");
+  require("../pengaturan/medoo.php");
+  require("../pengaturan/helper.php");
+  
+  $prepared_statement = [];
+  $result = null;
+  if(isset($_GET['id_posisi']))
+  {
+    $prepared_statement = ['id_posisi' => $_GET['id_posisi']];
+    $result = $db->get("tbl_jabatan", "*", $prepared_statement);
+  }
+  else
+  {
+    $result = $db->select("tbl_jabatan", "*");
+  }
+  
+  echo json_encode($result);
+  
+?>
